c52261658798ac6a73d72dfc4a01b84bc3eedeaf
tyler <tymaggio03@gmail.com>
2022-02-11 18:30:16 -0700

    commit tree 062e94ffcf1f908f7f6ddbeff93ca7d7180d996d

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,23 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  rescue_from ActiveRecord::RecordInvalid, with: :render_422_response
+
+  before_action :authorize_user
+
+  private 
+
+    def render_422_response(errors) 
+    render json: { errors: errors.record.errors.full_messages }, status: :unprocessable_entity
+  end  
+
+  # def current_user 
+  #   User.find_by(id: session[:current_user]) 
+  # end
+
+  def authorize_user
+    @current_user = User.find_by(id: session[:user_id])
+    return render json: { errors: ["Not authorized"] }, status: :unauthorized unless @current_user #session.include? :user_id
+  end
+
 end
